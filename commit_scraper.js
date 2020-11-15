6f761849cc799926c3d47fdcee97d4e002a241db
nimblebitpusher <micahrye@bitdust.local>
2020-11-15 04:08:12 -0500

    Cleaned up some cruft from script

--- commit_scraper.js
+++ commit_scraper.js
@@ -1,54 +1,11 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-const urls = [
-    "https://github.com/alex/nyt-2020-election-scraper/commits/master/results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+34&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+69&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+104&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+139&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+174&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+209&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+244&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+279&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+314&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+349&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+384&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+419&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+454&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+489&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+524&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+559&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+594&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+629&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+664&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+699&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+734&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+769&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+804&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+839&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+874&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+909&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+944&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+979&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1014&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1049&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1084&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1119&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1154&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1189&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1224&branch=master&path%5B%5D=results.json",
-    // "https://github.com/alex/nyt-2020-election-scraper/commits/master?after=bac635c7ac479d8d1ca3d63d53602a594c311ee6+1259&branch=master&path%5B%5D=results.json",
-]; 
- 
+
 (async() => {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    urls.forEach(url => {
-        console.log(url);
-    });
 
     let pageNumber = 1;
     let pageUrl = "https://github.com/alex/nyt-2020-election-scraper/commits/master/results.json";
